5e52b884137f34ea8c9539becefa4c634903e195
Giancarlo <giancarlo.birello@gmail.com>
2019-04-10 20:59:02 +0200

    Check manifest URL is valid

--- src/Plugin/Field/FieldFormatter/StrawberryPagedFormatter.php
+++ src/Plugin/Field/FieldFormatter/StrawberryPagedFormatter.php
@@ -234,7 +234,7 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
       ],
       'manifesturl_source' => [
         '#type' => 'textfield',
-        '#title' => t('JSON Key from where to fetch a full IIIF manifest URL'),
+        '#title' => t('JSON Key from where to fetch an absolute full IIIF manifest URL'),
         '#default_value' => $this->getSetting('manifesturl_source'),
         '#states' => [
           'visible' => [
@@ -423,14 +423,13 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
       // A rendered Manifest
       $manifest = '';
       switch ($pagestrategy) {
-        case 'json_key':
-
-          break;
+	case 'json_key':
+		break;
         case 'manifesturl':
-          $elements[$delta] = $this->processElementforManifestURL($delta, $jsondata, $item);
-          break;
+		$elements[$delta] = $this->processElementforManifestURL($delta, $jsondata, $item);
+		break;
         case 'metadatadisplayentity':
-          $elements[$delta] = $this->processElementforMetadatadisplays($delta, $jsondata, $item);
+          	$elements[$delta] = $this->processElementforMetadatadisplays($delta, $jsondata, $item);
       }
 
 
@@ -462,10 +461,7 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
       */
 
       $elements[$delta]['#attached']['library'][] = 'format_strawberryfield/iiif_iabookreader_strawberry';
-
-
     }
-
     return $elements;
   }
 
@@ -663,7 +659,6 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
         );
 
         $manifest = $manifestrenderelement->jsonSerialize();
-
         $groupid = 'iiif-' . $item->getName(
           ) . '-' . $nodeuuid . '-' . $delta . '-media';
         $htmlid = $groupid;
@@ -700,6 +695,7 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
         $element['media']['#attached']['drupalSettings']['format_strawberryfield']['iabookreader'][$htmlid]['height'] = max($max_height, 320);
       }
     }
+
     return $element;
   }
 
@@ -722,12 +718,12 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
     $max_height = $this->getSetting('max_height');
 
     if ($this->getSetting('manifesturl_source')) {
-	      $manifest_url_key = $this->getSetting('manifesturl_source');
-      	if ($jsondata[$manifest_url_key]) {
-		      $manifest_url = $jsondata[$manifest_url_key];
+	$manifest_url_key = $this->getSetting('manifesturl_source');
+      	if ($jsondata[$manifest_url_key]) {  
+	    $manifest_url = $jsondata[$manifest_url_key];
+            if (UrlHelper::isValid($manifest_url, TRUE)) {
         	$groupid = 'iiif-' . $item->getName() . '-' . $nodeuuid . '-' . $delta . '-media';
         	$htmlid = $groupid;
-
         	$element['media'] = [
           		'#type' => 'container',
           		'#default_value' => $htmlid,
@@ -756,12 +752,12 @@ class StrawberryPagedFormatter extends FormatterBase implements ContainerFactory
         	$element['media']['#attached']['drupalSettings']['format_strawberryfield']['iabookreader'][$htmlid]['manifesturl'] = $manifest_url;
         	$element['media']['#attached']['drupalSettings']['format_strawberryfield']['iabookreader'][$htmlid]['width'] = max($max_width, 400);
         	$element['media']['#attached']['drupalSettings']['format_strawberryfield']['iabookreader'][$htmlid]['height'] = max($max_height, 320);
+           }
       	}
     }
     return $element;
   }
 
-  
   /**
    * Use to process a Template directly.
    *
