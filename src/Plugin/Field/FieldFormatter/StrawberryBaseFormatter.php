59d01127b98d4309835676a328314ec1ee3cf42c
Diego Pino Navarro <dpino@metro.org>
2019-10-23 13:11:09 -0400

    Update StrawberryBaseFormatter.php
    Give each method proper PHPdocs. Remove unused USE statements, fix wrongly concatenated t() statements and use the correct placeholder replacements.
    Change all false and true to FALSE and TRUE and fix the Set Error By name to make sure we are really showing the error inline and not somewhere invisible on the top of the page when setting things. Use the Constants when calling validator and don't use/assume element name as a valid argument, we have now valid class Constants. Lastly, rename an unused (for now) method to correct camelcase (that was left from old code, not introduced here)

--- src/Plugin/Field/FieldFormatter/StrawberryBaseFormatter.php
+++ src/Plugin/Field/FieldFormatter/StrawberryBaseFormatter.php
@@ -1,14 +1,7 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: mlongley
- * Date: 9/25/19
- * Time: 8:56 PM
- */
 
 namespace Drupal\format_strawberryfield\Plugin\Field\FieldFormatter;
 
-use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FormatterBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Field\FieldDefinitionInterface;
@@ -19,31 +12,38 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\format_strawberryfield\Tools\IiifUrlValidator;
 use Drupal\Core\Access\AccessResult;
 
-
-
+/**
+ * StrawberryBaseFormatter base class for SBF/JSON based formatters.
+ */
 abstract class StrawberryBaseFormatter extends FormatterBase implements ContainerFactoryPluginInterface {
 
   /**
    * The Config for getting default IIIF settings.
    *
-   * @var \Drupal\Core\Config\ConfigFactoryInterface;
+   * @var \Drupal\Core\Config\ConfigFactoryInterface
    */
   protected $iiifConfig;
 
   /**
+   * StrawberryBaseFormatter Constructor.
+   *
    * @param string $plugin_id
+   *   The Plugin ID (Formatter).
    * @param mixed $plugin_definition
+   *   The Plugin Definition.
    * @param \Drupal\Core\Field\FieldDefinitionInterface $field_definition
    *   The definition of the field to which the formatter is associated.
    * @param array $settings
+   *   The main Settings.
    * @param string $label
    *   The formatter settings.
-   * @param $view_mode
+   * @param string $view_mode
    *   The view mode.
    * @param array $third_party_settings
    *   Any third party settings.
    *   The definition of the field to which the formatter is associated.
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
+   *   The ConfigFactory Container Interface.
    */
   public function __construct(
     $plugin_id,
@@ -51,7 +51,7 @@ abstract class StrawberryBaseFormatter extends FormatterBase implements Containe
     FieldDefinitionInterface $field_definition,
     array $settings,
     $label,
-    $view_mode,
+    string $view_mode,
     array $third_party_settings,
     ConfigFactoryInterface $config_factory
   ) {
@@ -83,25 +83,35 @@ abstract class StrawberryBaseFormatter extends FormatterBase implements Containe
     );
   }
 
+  /**
+   * {@inheritdoc}
+   */
   public static function defaultSettings() {
     return [
       'iiif_base_url' => \Drupal::config('format_strawberryfield.iiif_settings')->get('pub_server_url'),
       'iiif_base_url_internal' => \Drupal::config('format_strawberryfield.iiif_settings')->get('int_server_url'),
-      'use_iiif_globals' => true
-      ];
+      'use_iiif_globals' => TRUE,
+    ];
   }
 
   /**
-   * Fetches IIIF Urls from Config or settings depending on user input, and returns array of public and internal url.
+   * Fetches IIIF Urls from Config or settings depending on user input.
    *
    * @return array
-   *   The form of array is (public, internal)
+   *   And array in the form of:
+   *   [
+   *     'public' => 'a valid public URL',
+   *     'internal' => 'a valid internal URL'
+   *    ]
    */
   public function getIiifUrls() {
-    return [
-      'public' => boolval($this->getSetting('use_iiif_globals')) === true  ? $this->iiifConfig->get('pub_server_url') : rtrim($this->getSetting('iiif_base_url'), "/"),
-      'internal' => boolval($this->getSetting('use_iiif_globals')) === true ? $this->iiifConfig->get('int_server_url') : rtrim($this->getSetting('iiif_base_url_internal'), "/")
+
+    $urls = [
+      'public' => boolval($this->getSetting('use_iiif_globals')) === TRUE ? $this->iiifConfig->get('pub_server_url') : rtrim($this->getSetting('iiif_base_url'), "/"),
+      'internal' => boolval($this->getSetting('use_iiif_globals')) === TRUE ? $this->iiifConfig->get('int_server_url') : rtrim($this->getSetting('iiif_base_url_internal'), "/"),
     ];
+    return $urls;
+
   }
 
   /**
@@ -109,15 +119,15 @@ abstract class StrawberryBaseFormatter extends FormatterBase implements Containe
    */
   public function settingsSummary() {
     $summary = [];
-      $summary[] = $this->t('Use IIIF Global Urls? %value', [
-        '%value' => boolval($this->getSetting('use_iiif_globals')) === true  ? "Yes." : "No, use custom."
-      ]);
-      $summary[] = $this->t('IIIF Media Server base URI: %url', [
-        '%url' => $this->getIiifUrls()['public']
-      ]);
-      $summary[] = $this->t('IIIF Media Server Internal base URI: %url', [
-        '%url' => $this->getIiifUrls()['internal']
-      ]);
+    $summary[] = $this->t('Use IIIF Global Urls? %value', [
+      '%value' => boolval($this->getSetting('use_iiif_globals')) === TRUE ? "Yes." : "No, use custom.",
+    ]);
+    $summary[] = $this->t('IIIF Media Server base URI: %url', [
+      '%url' => $this->getIiifUrls()['public'],
+    ]);
+    $summary[] = $this->t('IIIF Media Server Internal base URI: %url', [
+      '%url' => $this->getIiifUrls()['internal'],
+    ]);
     return $summary;
   }
 
@@ -126,61 +136,83 @@ abstract class StrawberryBaseFormatter extends FormatterBase implements Containe
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
     $element['use_iiif_globals'] = [
-        '#type' => 'checkbox',
-        '#title' => t('Use Global IIIF Urls'),
-        '#description' => t('<b>Current Globals: </b><br>') . 'Public: '.$this->iiifConfig->get('pub_server_url') . '<br>Internal: ' . $this->iiifConfig->get('int_server_url'),
-        '#default_value' => $this->getSetting('use_iiif_globals'),
-        // Created custom 'data-*' selector rather than using name or id, because the name and id attributes are already set by Drupal and deeply nested for settingsForms. Overriding them causes problems with data submission. Trying to access them in #states is hard. So this is easier. This is valid HTML5.
-        '#attributes' => [
-          'data-checkbox-selector' => 'use_iiif_globals'
-        ],
-      ];
-    $element['iiif_base_url'] = [
-        '#type' => 'url',
-        '#title' => $this->t('Custom base public accessible URL of your IIIF Media Server. Trailing slashes will be removed.'),
-        '#default_value' => $this->getIiifUrls()['public'],
-        '#states' => [
-          'visible' => [
-            ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => true],
-          ],
-          'required' => [
-            ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => true],
-          ]
-        ],
-        '#element_validate' => [[$this, 'validateUrl']],
+      '#type' => 'checkbox',
+      '#title' => t('Use Global IIIF Urls'),
+      '#description' => t("<b>Current Globals: </b><br> Public: @pub <br>Internal: @int", [
+        '@pub' => $this->iiifConfig->get('pub_server_url'),
+        '@int' => $this->iiifConfig->get('int_server_url'),
+      ]),
+      '#default_value' => $this->getSetting('use_iiif_globals'),
+      // Created custom 'data-*' selector rather than using name
+      // or id, because the name and id attributes are already set by Drupal
+      // and deeply nested for settingsForms.
+      // Overriding them causes problems with data submission.
+      // Trying to access them in #states is hard. So this is easier.
+      // This is valid HTML5.
+      '#attributes' => [
+        'data-checkbox-selector' => 'use_iiif_globals',
+      ],
     ];
-      $element['iiif_base_url_internal'] = [
-        '#type' => 'url',
-        '#title' => $this->t('Custom base URL of your IIIF Media Server accessible from inside this Webserver. Trailing slashes will be removed.'),
-        '#default_value' => $this->getIiifUrls()['internal'],
-        '#states' => [
-          'visible' => [
-            ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => true],
-          ],
-          'required' => [
-            ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => true],
-          ]
+    $element['iiif_base_url'] = [
+      '#type' => 'url',
+      '#title' => $this->t('Custom base public accessible URL of your IIIF Media Server. Trailing slashes will be removed.'),
+      '#default_value' => $this->getIiifUrls()['public'],
+      '#states' => [
+        'visible' => [
+          ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => TRUE],
         ],
-        '#element_validate' => [[$this, 'validateUrl']],
-      ];
+        'required' => [
+          ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => TRUE],
+        ],
+      ],
+      '#element_validate' => [[$this, 'validateUrl']],
+    ];
+    $element['iiif_base_url_internal'] = [
+      '#type' => 'url',
+      '#title' => $this->t('Custom base URL of your IIIF Media Server accessible from inside this Webserver. Trailing slashes will be removed.'),
+      '#default_value' => $this->getIiifUrls()['internal'],
+      '#states' => [
+        'visible' => [
+          ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => TRUE],
+        ],
+        'required' => [
+          ':checkbox[data-checkbox-selector="use_iiif_globals"]' => ['unchecked' => TRUE],
+        ],
+      ],
+      '#element_validate' => [[$this, 'validateUrl']],
+    ];
 
     return $element;
   }
 
-  public function validateUrl(&$element, FormStateInterface $form_state, &$complete_form) {
+  /**
+   * Validates the IIIF Urls.
+   *
+   * @param array $element
+   *   The Form element.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   *   The Form State Object.
+   * @param array $complete_form
+   *   The complete form structure.
+   */
+  public function validateUrl(array &$element, FormStateInterface $form_state, array &$complete_form) {
     $parents = $element['#parents'];
-    $url_type = end($parents);
+    $url_type = end($element['#parents']);
+
     $checkbox = array_slice($parents, 0, -1);
     $checkbox[] = 'use_iiif_globals';
 
-    // Only validate if using locally defined urls. The globals have already been validated.
-    if (boolval($form_state->getValue($checkbox)) === false) {
+    // Only validate if using locally defined urls.
+    // The globals have already been validated.
+    if (boolval($form_state->getValue($checkbox)) === FALSE) {
       $validator = new IiifUrlValidator();
-      $error = $validator->checkUrl($element['#value'], $url_type);
-      if (!empty($error)) {
-        // todo: find the actual place in form_state to place error inline.
-        $form_state->setErrorByName($url_type, t("We could not contact your IIIF server: @error", [
-          '@error' => $error
+      $url_type = ($url_type == 'iiif_base_url_internal') ? $validator::IIIF_INTERNAL_URL_TYPE : $validator::IIIF_EXTERNAL_URL_TYPE;
+      $valid = $validator->checkUrl($element['#value'], $url_type);
+      if (!$valid) {
+        // @TODO find the actual place in form_state to place error inline.
+
+        $form_state->setErrorByName(implode('][', $parents), t("We could not contact your @urltype IIIF server", [
+          '@urltype' => $url_type,
         ]));
       }
     }
@@ -202,14 +234,14 @@ abstract class StrawberryBaseFormatter extends FormatterBase implements Containe
   }
 
   /**
-   * Tries to guess mimetype of external referenced Uris
+   * Tries to guess mimetype of external referenced Uris.
    *
    * @param string $uripath
-   *
+   *   A URL pointing to a file (hopefully)
    * @return string
-   *  A guessed Mimetype
+   *   A guessed Mimetype
    */
-  protected function guessMimeForExternalURI(string $uripath) {
+  protected function guessMimeForExternalUri(string $uripath) {
     return \Drupal::service('file.mime_type.guesser')->guess($uripath);
   }
 
