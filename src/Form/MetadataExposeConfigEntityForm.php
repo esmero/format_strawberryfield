a085e6c1e4b238d3c1775a7ee45394a97c28a451
Diego Pino Navarro <dpino@metro.org>
2019-10-28 10:29:22 -0400

    Validate the eentity reference on load

--- src/Form/MetadataExposeConfigEntityForm.php
+++ src/Form/MetadataExposeConfigEntityForm.php
@@ -85,7 +85,7 @@ class MetadataExposeConfigEntityForm extends EntityForm {
         '#title' => $this->t('The Metadata display Entity (Twig) to be used to generate data at this endpoint.'),
         '#target_type' => 'metadatadisplay_entity',
         '#selection_handler' => 'default:metadatadisplay',
-        '#validate_reference' => FALSE,
+        '#validate_reference' => TRUE,
         '#required' => TRUE,
         '#default_value' => (!$metadataconfig->isNew()) ? $metadataconfig->getMetadataDisplayEntity() : NULL,
       ],
