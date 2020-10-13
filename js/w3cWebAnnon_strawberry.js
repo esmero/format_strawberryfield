fbb6c132576ba950fede872882257305fda7822a
Diego Pino Navarro <dpino@metro.org>
2020-10-13 18:26:54 -0400

    Update w3cWebAnnon_strawberry.js
    Just remove the code. We will use this as a base JS for all w3c but not now. Leaving this here as boiler plate code?

--- js/w3cWebAnnon_strawberry.js
+++ js/w3cWebAnnon_strawberry.js
@@ -1,49 +1,13 @@
-(function ($, Drupal, drupalSettings, Mirador) {
+(function ($, Drupal, drupalSettings, Annotorious) {
 
     'use strict';
 
     Drupal.behaviors.format_strawberryfield_w3cwebannon_initiate = {
         attach: function(context, settings) {
-            $('.strawberry-mirador-item[data-iiif-infojson]').once('attache_mirador')
+            $('.strawberry-w3cannon-item[data-iiif-infojson]').once('attache_w3cannon')
                 .each(function (index, value) {
                     // Get the node uuid for this element
                     var element_id = $(this).attr("id");
                     // Check if we got some data passed via Drupal settings.
-                    if (typeof(drupalSettings.format_strawberryfield.mirador[element_id]) != 'undefined') {
-
-                        $(this).height(drupalSettings.format_strawberryfield.mirador[element_id]['height']);
-                        if (drupalSettings.format_strawberryfield.mirador[element_id]['width'] != '100%') {
-                            $(this).width(drupalSettings.format_strawberryfield.mirador[element_id]['width']);
-                        }
-                        // Defines our basic options for Mirador IIIF.
-                        var $options = {
-                            id: element_id,
-                            windows: [{
-                                manifestId: drupalSettings.format_strawberryfield.mirador[element_id]['manifesturl'],
-                                thumbnailNavigationPosition: 'far-bottom',
-                            }]
-                        };
-                        var $firstmanifest = [drupalSettings.format_strawberryfield.mirador[element_id]['manifesturl']];
-                        var $allmanifests = $firstmanifest.concat(drupalSettings.format_strawberryfield.mirador[element_id]['manifestother']);
-                        var $secondmanifest = drupalSettings.format_strawberryfield.mirador[element_id]['manifestother'].find(x=>x!==undefined);
-
-                        if (Array.isArray($allmanifests) && $allmanifests.length && typeof($secondmanifest) != 'undefined') {
-                            var $secondwindow = new Object();
-                            $secondwindow.manifestId = $secondmanifest;
-                            $secondwindow.thumbnailNavigationPosition = 'far-bottom';
-                            $options.windows.push($secondwindow);
-                            var $manifests = new Object();
-                            $allmanifests.forEach(manifestURL => {
-                                // TODO Provider should be passed by metadata at
-                                // \Drupal\format_strawberryfield\Plugin\Field\FieldFormatter\StrawberryMiradorFormatter::viewElements
-                                // Deal with this for Beta3
-                                $manifests[manifestURL] = new Object({'provider':'See Metadata'});
-                            })
-                            $options.manifests = $manifests;
-                        }
-                        //@TODO add an extra Manifests key with every other one so people can select the others.
-                        var miradorInstance = Mirador.viewer($options);
-                        console.log('initializing WC3 Annotation service')
-                    }
                 })}}
-})(jQuery, Drupal, drupalSettings, window.Mirador);
+})(jQuery, Drupal, drupalSettings, OpenSeadragon.Annotorious);
