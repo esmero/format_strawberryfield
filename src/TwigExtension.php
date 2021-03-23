163a728c0e7357103cd405cfc295116fb22d3178
Pat Dunlavey <pat@born-digital.com>
2021-03-23 10:13:51 -0400

    ISSUE-145 WIP --- attempt at error trapping [STILL untested]

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -48,57 +48,91 @@ class TwigExtension extends \Twig_Extension {
    * @return null|array
    *   An array of render arrays for the entities found, or NULL if the entity does not exist.
    */
-  public function load_entities_by_label(string $label, string $entity_type, string $bundle_identifier = '', $view_mode = 'default', $check_access = TRUE, $limit = 1, $langcode = NULL): ?array {
+  public function format_strawberryfield_load_entities_by_label(string $label, string $entity_type, string $bundle_identifier = '', $view_mode = 'default', $check_access = TRUE, $limit = 1, $langcode = NULL): ?array {
     $label = \Drupal::database()->escapeLike($label);
     $limit = min($limit, 100);
     /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
-    switch($entity_type) {
-      case 'node':
-        $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
-        $query->condition('title', $label, 'LIKE')
-          ->accessCheck($check_access)
-          ->range(0,$limit);
-        if($bundle_identifier) {
-          $query->condition('type', $bundle_identifier);
-        }
-        $ids = $query->execute();
-        break;
-      case 'taxonomy_term':
-        $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery();
-        $query->condition('name', $label, 'LIKE')
-          ->accessCheck($check_access)
-          ->range(0,$limit);
-        if($bundle_identifier) {
-          $query->condition('vid', $bundle_identifier);
-        }
-        $ids = $query->execute();
-        break;
-      case 'group':
-        $query = \Drupal::entityTypeManager()->getStorage('group')->getQuery();
-        $query->condition('label', $label, 'LIKE')
-          ->accessCheck($check_access)
-          ->range(0,$limit);
-        if($bundle_identifier) {
-          $query->condition('type', $bundle_identifier);
-        }
-        $ids = $query->execute();
-        break;
-      case 'user':
-        $query = \Drupal::entityTypeManager()->getStorage('user')->getQuery();
-        $query->condition('name', $label, 'LIKE')
-          ->accessCheck($check_access)
-          ->range(0,$limit);
-        $ids = $query->execute();
-        break;
+    try {
+      switch($entity_type) {
+        case 'node':
+          $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
+          $query->condition('title', $label, 'LIKE')
+            ->accessCheck($check_access)
+            ->range(0,$limit);
+          if($bundle_identifier) {
+            $query->condition('type', $bundle_identifier);
+          }
+          $ids = $query->execute();
+          break;
+        case 'taxonomy_term':
+          $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery();
+          $query->condition('name', $label, 'LIKE')
+            ->accessCheck($check_access)
+            ->range(0,$limit);
+          if($bundle_identifier) {
+            $query->condition('vid', $bundle_identifier);
+          }
+          $ids = $query->execute();
+          break;
+        case 'group':
+          $query = \Drupal::entityTypeManager()->getStorage('group')->getQuery();
+          $query->condition('label', $label, 'LIKE')
+            ->accessCheck($check_access)
+            ->range(0,$limit);
+          if($bundle_identifier) {
+            $query->condition('type', $bundle_identifier);
+          }
+          $ids = $query->execute();
+          break;
+        case 'user':
+          $query = \Drupal::entityTypeManager()->getStorage('user')->getQuery();
+          $query->condition('name', $label, 'LIKE')
+            ->accessCheck($check_access)
+            ->range(0,$limit);
+          $ids = $query->execute();
+          break;
+      }
+    }
+    catch(\Exception $exception) {
+      $responseMessage = $exception->getMessage();
+      $message = $this->t('@exception_type thrown in @file:@line while querying for @entity_type entity ids matching "@label". Message: @response',
+        [
+          '@exception_type' => get_class($exception),
+          '@file' => $exception->getFile(),
+          '@line' => $exception->getLine(),
+          '@entity_type' => $entity_type,
+          '@label' => $label,
+          '@response' => $responseMessage
+        ]);
+      \Drupal::logger('format_strawberryfield')->warning($message);
+      return NULL;
     }
 
     if(!empty($ids)) {
-      $entities = [];
-      $twig_tweak = new TwigTweakExtension();
-      foreach($ids as $id) {
-        $entities[$id] = $twig_tweak->drupalEntity($entity_type, $id, $view_mode, $langcode);
+      try {
+        $entities = [];
+        $twig_tweak = new TwigTweakExtension();
+        foreach ($ids as $id) {
+          $entities[$id] = $twig_tweak->drupalEntity($entity_type, $id,
+            $view_mode, $langcode);
+        }
+        return $entities;
       }
-      return $entities;
+      catch(\Exception $exception) {
+        $responseMessage = $exception->getMessage();
+        $message = $this->t('@exception_type thrown in @file:@line while attempting to render @entity_type with id @id using Drupal\twig_tweak\TwigExtension::drupalEntity. Message: @response',
+          [
+            '@exception_type' => get_class($exception),
+            '@file' => $exception->getFile(),
+            '@line' => $exception->getLine(),
+            '@entity_type' => $entity_type,
+            '@id' => $id,
+            '@response' => $responseMessage
+          ]);
+        \Drupal::logger('format_strawberryfield')->warning($message);
+        return NULL;
+
+      }
     }
 
     return NULL;
