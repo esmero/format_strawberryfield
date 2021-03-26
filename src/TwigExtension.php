102cf690ea9e9eaa265202246018d8a02d96d131
Pat Dunlavey <pat@born-digital.com>
2021-03-26 17:51:24 -0400

    ISSUE-145 --- bunch of rework: added getFunctions() method so that now twig templates validate when sbf_entity_ids_by_label is used removed the LIKE operator, which is not supported and not needed. The '=' operator is case-insensitive for text fields by default bunch of refactoring to eliminate redundant code eliminated accessCheck - it's on my default in entityQueries and safest to leave that way. eliminated language_code, which wasn't being used and for which we have no use case

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -26,6 +26,15 @@ class TwigExtension extends \Twig_Extension {
   }
 
   /**
+   * @inheritDoc
+   */
+  public function getFunctions() {
+    return [
+        new \Twig_SimpleFunction('sbf_entity_ids_by_label', [$this, 'entityIdsByLabel']),
+      ];
+  }
+
+  /**
    * Returns entity ids of entities with matching title/name/label.
    *
    * Supported entity types are node, taxonomy_term, group, and user.
@@ -36,80 +45,56 @@ class TwigExtension extends \Twig_Extension {
    *   The entity type.
    * @param  string  $bundle_identifier
    *   The entity bundle (may be empty)
-   * @param  bool  $check_access
-   *   Whether to check for entity access permission (default TRUE).
    * @param  int  $limit
    *   Restrict to number of results. Capped at no more than 100.
-   * @param  null  $langcode
-   *   (optional) For which language the entity should be rendered, defaults to
-   *   the current content language.
    *
    * @return null|array
    *   An array of render arrays for the entities found, or NULL if the entity does not exist.
    */
-  public function sbf_entity_ids_by_label(string $label, string $entity_type, string $bundle_identifier = '', $check_access = TRUE, $limit = 1, $langcode = NULL): ?array {
-    $label = \Drupal::database()->escapeLike($label);
-    $limit = min($limit, 100);
-    /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
-    try {
-      switch($entity_type) {
-        case 'node':
-          $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
-          $query->condition('title', $label, 'LIKE')
-            ->accessCheck($check_access)
-            ->range(0,$limit);
-          if($bundle_identifier) {
-            $query->condition('type', $bundle_identifier);
-          }
-          $ids = $query->execute();
-          break;
-        case 'taxonomy_term':
-          $query = \Drupal::entityTypeManager()->getStorage('taxonomy_term')->getQuery();
-          $query->condition('name', $label, 'LIKE')
-            ->accessCheck($check_access)
-            ->range(0,$limit);
-          if($bundle_identifier) {
-            $query->condition('vid', $bundle_identifier);
-          }
-          $ids = $query->execute();
-          break;
-        case 'group':
-          $query = \Drupal::entityTypeManager()->getStorage('group')->getQuery();
-          $query->condition('label', $label, 'LIKE')
-            ->accessCheck($check_access)
-            ->range(0,$limit);
-          if($bundle_identifier) {
-            $query->condition('type', $bundle_identifier);
-          }
-          $ids = $query->execute();
-          break;
-        case 'user':
-          $query = \Drupal::entityTypeManager()->getStorage('user')->getQuery();
-          $query->condition('name', $label, 'LIKE')
-            ->accessCheck($check_access)
-            ->range(0,$limit);
-          $ids = $query->execute();
-          break;
+  public function entityIdsByLabel(
+    string $label,
+    string $entity_type,
+    string $bundle_identifier = '',
+    $limit = 1
+  ): ?array {
+    $fields = [
+      'node' => ['title', 'type'],
+      'taxonomy_term' => ['name', 'vid'],
+      'group' => ['label', 'type'],
+      'user' => ['name', NULL],
+    ];
+    $label_field = $fields[$entity_type][0] ?? NULL;
+    if ($label_field) {
+      $bundle_field = $fields[$entity_type][1] ?? NULL;
+      $limit = min($limit, 100);
+      /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+      try {
+        $query = \Drupal::entityTypeManager()->getStorage('node')->getQuery();
+        $query->condition($label_field, $label);
+        $query->range(0, $limit);
+        if ($bundle_identifier && $bundle_field) {
+          $query->condition($bundle_field, $bundle_identifier);
+        }
+        $ids = $query->execute();
+
+      } catch (\Exception $exception) {
+        $responseMessage = $exception->getMessage();
+        $message = t('@exception_type thrown in @file:@line while querying for @entity_type entity ids matching "@label". Message: @response',
+          [
+            '@exception_type' => get_class($exception),
+            '@file' => $exception->getFile(),
+            '@line' => $exception->getLine(),
+            '@entity_type' => $entity_type,
+            '@label' => $label,
+            '@response' => $responseMessage,
+          ]);
+        \Drupal::logger('format_strawberryfield')->warning($message);
+        return NULL;
       }
-    }
 
-    catch(\Exception $exception) {
-      $responseMessage = $exception->getMessage();
-      $message = $this->t('@exception_type thrown in @file:@line while querying for @entity_type entity ids matching "@label". Message: @response',
-        [
-          '@exception_type' => get_class($exception),
-          '@file' => $exception->getFile(),
-          '@line' => $exception->getLine(),
-          '@entity_type' => $entity_type,
-          '@label' => $label,
-          '@response' => $responseMessage
-        ]);
-      \Drupal::logger('format_strawberryfield')->warning($message);
-      return NULL;
-    }
-
-    if(!empty($ids)) {
-      return $ids;
+      if (!empty($ids)) {
+        return $ids;
+      }
     }
     return NULL;
   }
