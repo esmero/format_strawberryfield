88749eece18afdb14a1071385a7b1615f4980dab
Pat Dunlavey <patdunlavey@gmail.com>
2021-04-02 16:15:13 -0400

    WIP: Issue 145 -- Add twig extension for load entity ids by entity label (#147)
    * ISSUE-145 --- Initial WIP for Add twig extension for load entity by label #145
    
    * ISSUE-145 --- refactoring to use \Drupal\entityQuery; nothing has been tested yet
    
    * ISSUE-145 --- WIP Still Not Tested... refactoring to use \Drupal::entityTypeManager()->getStorage('node')->getQuery(), and to use twig_tweak's TwigExtension::drupalEntity method.
    
    * ISSUE-145 WIP --- a bit of code tweaking
    
    * ISSUE-145 WIP --- re-added the limit cap of 100
    
    * ISSUE-145 WIP --- attempt at error trapping [STILL untested]
    
    * ISSUE-145 --- simplified to just returning a list of entity ids. If the use case requires data from the entity, the twig template can use a twig_tweaks or bamboo_twig_load extension after this one.
    
    * ISSUE-145 --- removed drupal/twig_tweak dependency
    
    * ISSUE-145 --- bunch of rework:
    added getFunctions() method so that now twig templates validate when sbf_entity_ids_by_label is used
    removed the LIKE operator, which is not supported and not needed. The '=' operator is case-insensitive for text fields by default
    bunch of refactoring to eliminate redundant code
    eliminated accessCheck - it's on my default in entityQueries and safest to leave that way.
    eliminated language_code, which wasn't being used and for which we have no use case
    
    * ISSUE-145 --- removed unused Drupal\Core\Access\AccessResult and Drupal\Core\Cache\CacheableMetadata; some automatic code cleanup
    
    * ISSUE-145 --- few more tweaks after review
    
    * Just tiny checks and DCS and PHPDOCS
    
    @patdunlavey these are just tiny DCS, some trims, some casting . I rolled back some stupid corrections i made that were not even needed. You did good here!
    
    Co-authored-by: Pat Dunlavey <pat@born-digital.com>
    Co-authored-by: Diego Pino Navarro <dpino@metro.org>

--- src/TwigExtension.php
+++ src/TwigExtension.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Drupal\format_strawberryfield;
+
 use Twig\TwigTest;
 
 /**
@@ -9,17 +11,98 @@ use Twig\TwigTest;
  */
 class TwigExtension extends \Twig_Extension {
 
-  public function getTests(): array
-  {
+  public function getTests(): array {
     return [
       new TwigTest('instanceof', [$this, 'is_instanceof']),
     ];
   }
 
-  public function is_instanceof($value, string $type): bool
-  {
-    return ('null' === $type && null === $value)
-      || (\function_exists($func = 'is_'.$type) && $func($value))
+  /**
+   * @param $value
+   * @param  string  $type
+   *
+   * @return bool
+   */
+  public function is_instanceof($value, string $type): bool {
+    return ('null' === $type && NULL === $value)
+      || (\function_exists($func = 'is_' . $type) && $func($value))
       || $value instanceof $type;
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function getFunctions() {
+    return [
+      new \Twig_SimpleFunction('sbf_entity_ids_by_label',
+        [$this, 'entityIdsByLabel']),
+    ];
+  }
+
+  /**
+   * Returns entity ids of entities with matching title/name/label.
+   *
+   * @param string $label
+   *   The entity label that we're looking for
+   * @param string $entity_type
+   *   The entity type.
+   *   Supported entity types are node, taxonomy_term, group, and user.
+   * @param string $bundle_identifier
+   *   The entity bundle (may be empty)
+   * @param int $limit
+   *   Restrict to number of results. Capped at no more than 100.
+   *
+   * @return null|array
+   *   An array of Entity IDs for the entities found, or NULL if no match.
+   */
+  public function entityIdsByLabel(
+    string $label,
+    string $entity_type,
+    string $bundle_identifier = '',
+    $limit = 1
+  ): ?array {
+    $fields = [
+      'node' => ['title', 'type'],
+      'taxonomy_term' => ['name', 'vid'],
+      'group' => ['label', 'type'],
+      'user' => ['name', NULL],
+    ];
+    $limit = (int) $limit;
+    $entity_type = trim($entity_type);
+    $bundle_identifier = trim($bundle_identifier);
+    $label_field = $fields[$entity_type][0] ?? NULL;
+    if ($label_field) {
+      $bundle_field = $fields[$entity_type][1] ?? NULL;
+      $limit = min((int) $limit, 100);
+      $label = trim($label);
+      try {
+        /** @var \Drupal\Core\Entity\Query\QueryInterface $query */
+        $query = \Drupal::entityTypeManager()->getStorage($entity_type)->getQuery();
+        $query->condition($label_field, $label);
+        if ($bundle_identifier && $bundle_field) {
+          $query->condition($bundle_field, $bundle_identifier);
+        }
+        $query->range(0, $limit);
+        $ids = $query->execute();
+      }
+      catch (\Exception $exception) {
+        $message = t('@exception_type thrown in @file:@line while querying for @entity_type entity ids matching "@label". Message: @response',
+          [
+            '@exception_type' => get_class($exception),
+            '@file' => $exception->getFile(),
+            '@line' => $exception->getLine(),
+            '@entity_type' => $entity_type,
+            '@label' => $label,
+            '@response' => $exception->getMessage(),
+          ]);
+        \Drupal::logger('format_strawberryfield')->warning($message);
+        return NULL;
+      }
+
+      if (!empty($ids)) {
+        return $ids;
+      }
+    }
+    return NULL;
   }
 }
