b27b7340343558219d6946b0f780350baae344d2
Diego Pino Navarro <dpino@metro.org>
2022-07-06 23:28:30 -0400

    Leaflet SBF Views + Pannellum + leaflet interactions

--- src/Plugin/Field/FieldFormatter/StrawberryMapFormatter.php
+++ src/Plugin/Field/FieldFormatter/StrawberryMapFormatter.php
@@ -510,7 +510,7 @@ class StrawberryMapFormatter extends StrawberryBaseFormatter implements Containe
   public function viewElements(FieldItemListInterface $items, $langcode) {
     $elements = [];
     $max_width = $this->getSetting('max_width');
-    $max_width_css = empty($max_width) || $max_width == 0 ? '100%' : $max_width .'px';
+    $max_width_css = empty($max_width) || (int) $max_width == 0 ? '100%' : $max_width .'px';
     $max_height = $this->getSetting('max_height');
     $mediasource = is_array($this->getSetting('mediasource')) ? $this->getSetting('mediasource') : [];
     $main_mediasource = $this->getSetting('main_mediasource');
