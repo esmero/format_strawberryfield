192cfcfd368edb5d2f241f0c3e501ac7d5075900
Diego Pino Navarro <dpino@metro.org>
2023-01-17 20:51:26 -0300

    More checks because JS is weird and so is Drupal...

--- modules/format_strawberryfield_views/js/modal-exposed-form-ajax.js
+++ modules/format_strawberryfield_views/js/modal-exposed-form-ajax.js
@@ -67,26 +67,29 @@
           // Check which modal blocks (from the settings)
           // belong to it.
           // Do what Drupal.views.ajaxView.prototype.attachExposedFormAjax does differently
-
-          Object.keys(drupalSettings.views.ajaxViews || {}).forEach((i) => {
-            //var block_settings = drupalSettings.format_strawberryfield_views.modal_exposed_form_block[$modal_exposed_form_id];
-            if (Drupal.views.instances[i].settings.view_name == $view_parts[0] //block_settings.view_id
-              && Drupal.views.instances[i].settings.view_display_id == $view_parts[1]) { //block_settings.current_display_id) {
-              var exposed_form_selector = '#views-exposed-form-' + $view_parts[0].replace(/_/g, '-') + '-' +$view_parts[1].replace(/_/g, '-');
-              var $exposed_form = $(exposed_form_selector).length;
-              if ($exposed_form > 0) {
-                $exposed_form = 1 ;
+          if (typeof(drupalSettings.views) != 'undefined') {
+            Object.keys(drupalSettings.views.ajaxViews || {}).forEach((i) => {
+              //var block_settings = drupalSettings.format_strawberryfield_views.modal_exposed_form_block[$modal_exposed_form_id];
+              if ((typeof (Drupal.views.instances[i]) !== 'undefined') && Drupal.views.instances[i].hasOwnProperty("settings")) {
+                if (Drupal.views.instances[i].settings.view_name == $view_parts[0] //block_settings.view_id
+                  && Drupal.views.instances[i].settings.view_display_id == $view_parts[1]) { //block_settings.current_display_id) {
+                  var exposed_form_selector = '#views-exposed-form-' + $view_parts[0].replace(/_/g, '-') + '-' + $view_parts[1].replace(/_/g, '-');
+                  var $exposed_form = $(exposed_form_selector).length;
+                  if ($exposed_form > 0) {
+                    $exposed_form = 1;
+                  }
+                  $('input[type=submit], button[type=submit], input[type=image]', $modal_exposed_form).not('[data-drupal-selector=edit-reset]').each(function (index) {
+                    var selfSettings = $.extend({}, Drupal.views.instances[i].element_settings, {
+                      base: $(this).attr('id'),
+                      element: this
+                    });
+                    selfSettings.submit.exposed_form_display = $exposed_form;
+                    $modal_exposed_form.exposedFormAjax[index] = Drupal.ajax(selfSettings);
+                  });
+                }
               }
-              $('input[type=submit], button[type=submit], input[type=image]', $modal_exposed_form).not('[data-drupal-selector=edit-reset]').each(function (index) {
-                var selfSettings = $.extend({}, Drupal.views.instances[i].element_settings, {
-                  base: $(this).attr('id'),
-                  element: this
-                });
-                selfSettings.submit.exposed_form_display = $exposed_form;
-                $modal_exposed_form.exposedFormAjax[index] = Drupal.ajax(selfSettings);
-              });
-            }
-          });
+            });
+          }
         });
       }
 
