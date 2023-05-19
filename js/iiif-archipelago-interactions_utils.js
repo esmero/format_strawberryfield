c40dfa9a6380d23ed57f08f0b69cb9f2f4d0d1c3
Diego Pino Navarro <dpino@metro.org>
2023-05-18 22:10:22 -0400

    More JS stuff. GOSH

--- js/iiif-archipelago-interactions_utils.js
+++ js/iiif-archipelago-interactions_utils.js
@@ -3,8 +3,6 @@
   'use strict';
   var FormatStrawberryfieldIiifUtils = {
 
-    wasConfirmed: false,
-
     dispatchAdoChange: function(el, nodeids){
       /* el being a dom document via const el = document.getElementById(element_id);*/
       /* nodeid being the ADO Node ID */
@@ -15,9 +13,9 @@
       else {
         nodeidsArray = nodeids
       }
-        const event = new CustomEvent('sbf:ado:change', { bubbles: true, detail: {nodeid: nodeidsArray} });
-        console.log(event);
-        el.dispatchEvent(event);
+      const event = new CustomEvent('sbf:ado:change', { bubbles: true, detail: {nodeid: nodeidsArray} });
+      console.log(event);
+      el.dispatchEvent(event);
       return this;
     },
 
@@ -29,7 +27,37 @@
       console.log(event);
       el.dispatchEvent(event);
       return this;
+    },
+    fetchIIIFManifest: function (iiifmanifesturl) {
+      fetch(iiifmanifesturl)
+        .then(res => res.json())
+        .then(function (res) {
+          return res;
+        })
+        .catch(function() {
+          // Die as silent as possible but still log a thing.
+          console.log("Error fetching IIIF Manifest " + iiifmanifesturl);
+          return {};
+        });
+    },
+    getGeoAnnotations: function (iiifmanifest) {
+      // this.items[0].annotations[0].items[0].motivation
+      let body = {};
+      target = {};
+         if (iiifmanifest?.items) {
+           iiifmanifest?.items.forEach(items => {
+             if (Array.isArray(items?.annotations)) {
+               items.annotations.find(function (b) {
+                 if (b?.motivation == 'georeferencing') {
+                   body = b?.body;
+                   target = b?.target; // will contain selector and source
+                 }
+               });
+             }
+           });
+         }
     }
+
   };
 
   /* Make it part of the Global Drupal Object */
