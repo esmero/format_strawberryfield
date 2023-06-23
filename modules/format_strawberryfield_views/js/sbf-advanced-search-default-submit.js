31548a5f424cddd1380f1d5c924dd70d7bde47b9
Diego Pino Navarro <dpino@metro.org>
2023-06-22 23:26:38 -0400

    Pure JS deals with removal/adding of new fields
    This will eventually in the future allow what users want... 80's Advanced Searches. Gets around Drupal's Core bug that does not allow #ajax actions to work on exposed form filters bc the Filter logic removes the $form_id needed for the caching system to work (throwing an invisible error so weird... like max file upload reached? c'mon drupal)

--- modules/format_strawberryfield_views/js/sbf-advanced-search-default-submit.js
+++ modules/format_strawberryfield_views/js/sbf-advanced-search-default-submit.js
@@ -8,9 +8,44 @@
           defaultSubmitInput.click();
         }
       };
+
+      var addMoreInterception = function(defaultSubmitInput, ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        const $form = defaultSubmitInput.closest('.views-exposed-form');
+        let $count =  $form.querySelector('input[name="sbf_advanced_search_api_fulltext_advanced_search_fields_count"]');
+        $count.value = Number($count.value) + 1;
+        defaultSubmitInput.click();
+      };
+
+      var delOneInterception = function(defaultSubmitInput, ev) {
+        ev.preventDefault();
+        ev.stopPropagation();
+        if (typeof ev.target.dataset.advancedSearchPrefix !== 'undefined') {
+          const $name = ev.target.dataset.advancedSearchPrefix + '_advanced_search_fields_count';
+          const $form = defaultSubmitInput.closest('.views-exposed-form');
+          let $count = $form.querySelector('input[name="'+$name+'"]');
+          if ($count) {
+            $count.value = Number($count.value) - 1;
+            const $tounsetSelector = ev.target.dataset.advancedSearchPrefix + '_' + $count.value;
+            let $tounset = $form.querySelector('input[name="' + $tounsetSelector + '"]');
+            if ($tounset) {
+              $tounset.value = '';
+            }
+          }
+        }
+        defaultSubmitInput.click();
+      };
+
       for (const defaultSubmitInput of document.querySelectorAll('.views-exposed-form [data-default-submit]')) {
         for (const formInput of defaultSubmitInput.form.querySelectorAll('input')) {
           formInput.addEventListener('keypress', formInterception.bind(null, defaultSubmitInput));
+        }
+        for (const addMore of document.querySelectorAll('.views-exposed-form [data-advanced-search-addone]')) {
+          addMore.addEventListener('click', addMoreInterception.bind(null, defaultSubmitInput));
+        }
+        for (const delOne of document.querySelectorAll('.views-exposed-form [data-advanced-search-delone]')) {
+          delOne.addEventListener('click', delOneInterception.bind(null, defaultSubmitInput));
         }
       }
     }
