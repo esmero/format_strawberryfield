38d9196bc7b01b2b4ce4f51630b931d3e85549cf
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2023-12-30 18:40:05 -0500

    Adds View Modes for Nodes now

--- src/Form/MetadataDisplayUsageForm.php
+++ src/Form/MetadataDisplayUsageForm.php
@@ -2,10 +2,12 @@
 namespace Drupal\format_strawberryfield\Form;
 use Drupal\ami\Entity\amiSetEntity;
 use Drupal\Component\Plugin\Exception\PluginException;
+use Drupal\Core\Entity\Entity\EntityViewDisplay;
 use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
 use Drupal\format_strawberryfield\MetadataDisplayInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Drupal\Core\Config\ConfigFactoryInterface;
@@ -85,15 +87,15 @@ class MetadataDisplayUsageForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state, MetadataDisplayInterface $metadatadisplay_entity = NULL) {
 
-    // Start with Exposed Metadata display entities
-    if ($metadatadisplay_entity) {
 
+    if ($metadatadisplay_entity) {
+      // Start with Exposed Metadata display entities
       $form['metadatadisplay_usage']['metadataexpose_entity'] = [
         '#type' => 'fieldset',
         '#title' => $this->t('Exposed Metadata Display Entities using @label', [
           '@label' => $metadatadisplay_entity->label()
         ]),
-        'table' => [ '#type' => 'table',
+        'table' => ['#type' => 'table',
           '#prefix' => '<div id="table-fieldset-wrapper">',
           '#suffix' => '</div>',
           '#header' => [
@@ -111,12 +113,13 @@ class MetadataDisplayUsageForm extends FormBase {
         }
       }
       try {
+        // AMI sets
         $form['metadatadisplay_usage']['ami_set_entity'] = [
           '#type' => 'fieldset',
           '#title' => $this->t('AMI sets using @label', [
             '@label' => $metadatadisplay_entity->label()
           ]),
-          'table' => [ '#type' => 'table',
+          'table' => ['#type' => 'table',
             '#prefix' => '<div id="table-fieldset-wrapper">',
             '#suffix' => '</div>',
             '#header' => [
@@ -157,14 +160,71 @@ class MetadataDisplayUsageForm extends FormBase {
             $form['metadatadisplay_usage']['ami_set_entity']['table'][$ami_entity->id()]['label'] = $ami_entity->toLink($this->t('Edit @label', ['@label' => $ami_entity->label()]), 'edit-form')->toRenderable();
           }
         }
-      }
-      catch (PluginException) {
+      } catch (PluginException) {
         // Means AMI module is not installed, the AMI type entity does not exist. That is Ok.
         // simply no output
       }
-    }
 
-    $form['metadatadisplay_usage']['#markup'] = 'Settings form for Metadata Display Entity. Manage field settings here.';
-    return $form;
+
+      // Now Entity view display configs for nodes.
+
+      $form['metadatadisplay_usage']['entity_view'] = [
+        '#type' => 'fieldset',
+        '#title' => $this->t('Entity View Modes using @label', [
+          '@label' => $metadatadisplay_entity->label()
+        ]),
+        'table' => ['#type' => 'table',
+          '#prefix' => '<div id="table-fieldset-wrapper">',
+          '#suffix' => '</div>',
+          '#header' => [
+            $this->t('Label'),
+            $this->t('Replace'),
+          ],
+          '#empty' => $this->t('No usage.'),
+        ]
+      ];
+      // Drupal... gosh.
+      // We need to get first all view modes
+      $view_modes = \Drupal::service('entity_display.repository')->getViewModes('node');
+
+      foreach ($this->configFactory()->listAll('core.entity_view_display.node.') as $entity_view_display_config) {
+        $entity_view = $this->configFactory()->get($entity_view_display_config);
+        $in_use = FALSE;
+        if ($entity_view) {
+          $data = $entity_view->getRawData();
+          if (isset($data['third_party_settings']['ds']['fields'])) {
+            foreach ($data['third_party_settings']['ds']['fields'] as &$field) {
+              if (($field['settings']['formatter']['metadatadisplayentity_uuid'] ?? NULL) == $metadatadisplay_entity->uuid()) {
+                $in_use = TRUE;
+              }
+            }
+          }
+          if (isset($data['content'])) {
+            foreach ($data['content'] as &$realfield) {
+              if (($realfield['settings']['metadatadisplayentity_uuid'] ?? NULL) == $metadatadisplay_entity->uuid()) {
+                $in_use = TRUE;
+              }
+            }
+          }
+          if ($in_use) {
+            $entity_view_display_storage = $this->entityTypeManager->getStorage('entity_view_display');
+            /** @var EntityViewDisplay $entity_view_display */
+            $entity_view_display = $entity_view_display_storage->load($entity_view->get('id'));
+            $bundle = $entity_view_display->getTargetBundle();
+            // Default has no label... just named default
+            $label = $view_modes[$entity_view_display->getMode()]['label'] ?? 'Default';
+            $entity_view_display_link = Link::createFromRoute($this->t('Edit @entity_view_display_label',['@entity_view_display_label' => $label]), "entity.entity_view_display.node.view_mode", [
+              'entity_type_id' => 'node',
+              'node_type' => $bundle,
+              'view_mode_name' => $entity_view_display->getMode()
+            ]);
+            // Special parameter used to easily recognize all Field UI routes.
+            $form['metadatadisplay_usage']['entity_view']['table'][$entity_view_display->id()]['label'] = $entity_view_display_link->toRenderable();
+          }
+        }
+      }
+    }
+      $form['metadatadisplay_usage']['#markup'] = 'Settings form for Metadata Display Entity. Manage field settings here.';
+      return $form;
+    }
   }
-}
