1528e731535faa04ec6740dd543cbcf0e9eff96c
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-01-19 10:19:46 -0500

    And.. done!
    Yay diego, yay

--- modules/format_strawberryfield_views/js/sbf-advanced-search-default-submit.js
+++ modules/format_strawberryfield_views/js/sbf-advanced-search-default-submit.js
@@ -105,50 +105,63 @@
                         });
                       }
 
-                    // Now, shift values one step back
-                    if ($not_hidden_one.length > 0) {
-                      let previous = $to_clear;
-                      let found = false;
-                      [].forEach.call($not_hidden_one, function(el) {
-                        if (!found && el.id == $to_clear.id) {
-                          found = true;
-                          // that way we skip this one too.
-                          return;
+                      // Now, shift values one step back
+                      if ($not_hidden_one.length > 0) {
+                        let previous = $to_clear;
+                        let found = false;
+                        [].forEach.call($not_hidden_one, function(el) {
+                          if (!found && el.id == $to_clear.id) {
+                            found = true;
+                            // that way we skip this one too.
+                            return;
+                          }
+                          if (!found) {
+                            return;
+                          }
+                          const $tocopy = el.querySelector('input[name^="' +  ev.target.dataset.advancedSearchPrefix + '"]');
+                          const $tocopyinto = previous.querySelector('input[name^="' + ev.target.dataset.advancedSearchPrefix + '"]');
+                          if ($tocopy && $tocopyinto) {
+                            $tocopyinto.value = $tocopy.value;
+                          }
+                          let $tocopyselect = el.querySelectorAll('select[name^="' +  ev.target.dataset.advancedSearchPrefix + '"]');
+                          if ($tocopyselect.length > 0 && $tocopy) {
+                            [].forEach.call($tocopyselect, function(select) {
+                              // fetch the same by data attribute from the previous
+                              const $which = select.dataset.advancedSearchType;
+                              const $sameSelectTo = previous.querySelector('[data-advanced-search-type="'+ $which + '"]');
+                              const currentSelectValue = select.selectedIndex;
+                              if (currentSelectValue && $sameSelectTo) {
+                                $sameSelectTo.selectedIndex = currentSelectValue
+                              }
+                              else if ($sameSelectTo) {
+                                //if null set to the first
+                                $sameSelectTo.selectedIndex = 0;
+                              }
+                            });
+                          }
+                          previous = el;
+                        });
+                        // Then hide the last.
+                        const $last_not_hidden_one = $not_hidden_one[$not_hidden_one.length - 1];
+                        $last_not_hidden_one.classList.toggle('hidden');
+                        // name^= means starts with.
+                        let $tounset = $last_not_hidden_one.querySelector('input[name^="' +  ev.target.dataset.advancedSearchPrefix + '"]');
+                        if ($tounset) {
+                          $tounset.value = '';
                         }
-                        if (!found) {
-                          return;
-                        }
-                        const $tocopy = el.querySelector('input[name^="' +  ev.target.dataset.advancedSearchPrefix + '"]');
-                        const $tocopyinto = previous.querySelector('input[name^="' + ev.target.dataset.advancedSearchPrefix + '"]');
-                        if ($tocopy && $tounset) {
-                          $tocopyinto.value = $tocopy.value;
-                        }
-                        let $tocopyselect = el.querySelectorAll('select[name^="' +  ev.target.dataset.advancedSearchPrefix + '"]');
-                        if ($tocopyselect.length > 0 && $tocopy) {
-                          [].forEach.call($tocopyselect, function(select) {
-                            // fetch the same by data attribute from the previous
-                            const $which = select.dataset.advancedSearchType;
-                            const $sameSelectTo = previous.querySelector('[data-advanced-search-type="'+ $which + '"]');
-                            const currentSelectValue = select.selectedIndex;
-                            if (currentSelectValue && $sameSelectTo) {
-                              $sameSelectTo.selectedIndex = currentSelectValue
-                            }
-                            else if ($sameSelectTo) {
-                              //if null set to the first
-                              $sameSelectTo.selectedIndex = 0;
-                            }
+                        let $tounsetselect = $last_not_hidden_one.querySelectorAll('select[name^="' +  ev.target.dataset.advancedSearchPrefix + '"]');
+                        if ($tounsetselect.length > 0) {
+                          [].forEach.call($tounsetselect, function(el) {
+                            // -1 means nothing which is not the drupal default...
+                            el.selectedIndex = 0;
                           });
                         }
-                        previous = el;
-                      });
-                    }
-                    // Then hide the last.
+                      }
                     }
                   }
-
-
                 }
-                if ($count.value == Number(ev.target.dataset.advancedSearchMin)) {
+                if ($count.value == Number(ev.target.dataset.advancedSearchMin) && ev.target.dataset.advancedSearchTarget != "self") {
+                  // Don't hide if we are using individual buttons, the last wrapper container already gets hidden.
                   ev.target.classList.add('hidden');
                 }
                 if ($count.value < Number(ev.target.dataset.advancedSearchMax)) {
@@ -159,6 +172,7 @@
                 }
               }
               else {
+                // Normal behavior. Simple stuff
                 const $tounsetSelector = ev.target.dataset.advancedSearchPrefix + '_' + $count.value;
                 let $tounset = $form.querySelector('input[name="' + $tounsetSelector + '"]');
                 if ($tounset) {
