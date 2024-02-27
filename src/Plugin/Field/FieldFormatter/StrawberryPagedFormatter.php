edc7cfd926639e79b834f28214918a8c88849187
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-02-27 13:52:54 -0500

    fixes PHP doc for processElementforManifestURL

--- src/Plugin/Field/FieldFormatter/StrawberryPagedFormatter.php
+++ src/Plugin/Field/FieldFormatter/StrawberryPagedFormatter.php
@@ -8,6 +8,8 @@
 
 namespace Drupal\format_strawberryfield\Plugin\Field\FieldFormatter;
 
+use Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException;
+use Drupal\Component\Plugin\Exception\PluginNotFoundException;
 use Drupal\Core\Cache\Cache;
 use Drupal\Core\Field\FieldItemListInterface;
 use Drupal\Core\Field\FieldItemInterface;
@@ -651,11 +653,11 @@ class StrawberryPagedFormatter extends StrawberryBaseFormatter implements Contai
    *
    * @param int $delta
    * @param array $jsondata
-   * @param \Drupal\Core\Field\FieldItemInterface $item
-   *
+   * @param FieldItemInterface $item
+   * @param string $pagestrategy
    * @return array
-   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
-   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws InvalidPluginDefinitionException
+   * @throws PluginNotFoundException
    */
   public function processElementforManifestURL($delta, array $jsondata, FieldItemInterface $item, string $pagestrategy) {
     $delta = $delta ?? 0;
