64f985169cdee44d87bd86b69b5c369639861001
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2024-10-24 13:53:27 -0400

    Update slider via inputs

--- modules/format_strawberryfield_facets/js/slider.js
+++ modules/format_strawberryfield_facets/js/slider.js
@@ -77,7 +77,7 @@
 
       $.extend(defaults, slider_settings);
 
-      $('.sbf-date-facet-slider', widget).slider(defaults)
+      var $slider = $('.sbf-date-facet-slider', widget).slider(defaults)
         .slider('pips', {
           prefix: slider_settings.prefix,
           suffix: slider_settings.suffix
@@ -87,6 +87,59 @@
           suffix: slider_settings.suffix,
           labels: slider_settings.labels
         });
+      var changeHandlerInput = function (e) {
+        //e.preventDefault();
+        console.log(e);
+        let error = Drupal.t("Wrong date");
+        const slider = widget.querySelector('.sbf-date-facet-slider')
+        let min_timestamp =  slider.dataset.min;
+        let max_timestamp =  slider.dataset.max;
+        let min = $slider.slider('option').min;
+        let max = $slider.slider('option').max;
+        let ui_min = $slider.slider('option').values[0];
+        let ui_max = $slider.slider('option').values[1];
+        if  (!e.target.checkValidity()) {
+          e.target.reportValidity()
+          return;
+        }
+        if (e.target.type == "number") {
+          if (e.target.dataset?.type == "date-range-min") {
+            min = ui_min = parseInt(e.target.value);
+            min_timestamp =  toTimestamp(min);
+          }
+          else {
+            max = ui_max = parseInt(e.target.value);
+            max_timestamp =  toTimestamp(max);
+          }
+        }
+        else if (e.target.type == "date") {
+          if (e.target.dataset?.type == "date-range-min") {
+            min = ui_min =new Date(e.target.value).getFullYear();
+            min_timestamp = toTimestamp(e.target.value);
+          }
+          else {
+            max = ui_max = new Date(e.target.value).getFullYear();
+            max_timestamp = toTimestamp(e.target.value);
+          }
+        }
+        if ($slider.slider('option').min > min || $slider.slider('option').max < max) {
+          e.target.reportValidity()
+        }
+        else {
+          $slider.slider('option', 'values', [ui_min, ui_max]).slider("pips", "refresh").slider("float", "refresh");
+          slider.dataset.min = min_timestamp;
+          slider.dataset.max = max_timestamp;
+        }
+
+
+        //const min = toTimestamp( ui.values[0]);
+        //const max = toTimestamp( ui.values[1]);
+        //slider.dataset.min = min;
+        //slider.dataset.max = max;
+      };
+      $("input.facet-date-range.form-control", widget).on("change", changeHandlerInput);
+
+
     };
 
   })(jQuery);
