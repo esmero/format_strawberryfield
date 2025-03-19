b766f785c96bda535dbd878f1fc2f20ec5e63da3
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2025-03-19 10:15:40 -0400

    Simplify the Ajax Facet block controller override
    Don't recalculate the Libraries, re-use the ajax page state (which someday i will understand?)
    This solves #507

--- modules/format_strawberryfield_facets/src/Controller/SbfFacetBlockAjaxController.php
+++ modules/format_strawberryfield_facets/src/Controller/SbfFacetBlockAjaxController.php
@@ -26,10 +26,13 @@ class SbfFacetBlockAjaxController extends FacetBlockAjaxController {
    *   Thrown when the view was not found.
    */
   public function ajaxFacetBlockView(Request $request) {
+    // The parent method provides the actual block rendering of the facet.
     $response = parent::ajaxFacetBlockView($request);
 
-    /* Changes are Facet has not been patched upstream for missing session */
+    /* Start patching
+    // Changes are Facet has not been patched upstream for missing session */
     // @TODO: remove in 1.6.0 once https://www.drupal.org/files/issues/2024-08-10/3052574-session-not-set-warning-251.patch
+    // and https://www.drupal.org/files/issues/2024-08-20/facets-add-missing-ajax_page_state-3466566-2.0.8.patch
     // comes in.
     $path = $request->request->get('facet_link');
     $new_request = Request::create($path);
@@ -38,37 +41,20 @@ class SbfFacetBlockAjaxController extends FacetBlockAjaxController {
 
     $processed = $this->pathProcessor->processInbound($path, $new_request);
     $processed_request = Request::create($processed);
-
     $this->currentPath->setPath($processed_request->getPathInfo());
     $request->attributes->add($this->router->matchRequest($new_request));
     $this->currentRouteMatch->resetRouteMatch();
+    // Add ajax_page_state to the new request if set.
+    if ($request->request->has('ajax_page_state')) {
+      $new_request->request->set('ajax_page_state', $request->request->all('ajax_page_state'));
+    }
+    elseif ($request->query->has('ajax_page_state')) {
+     $new_request->query->set('ajax_page_state', $request->query->all('ajax_page_state'));
+    }
     $request_stack->push($new_request);
-
     $container = \Drupal::getContainer();
     $container->set('request_stack', $request_stack);
     /* end patching */
-
-    $facets_blocks = $request->request->all()['facets_blocks'] ?? [];
-    // Make sure we are not updating blocks multiple times.
-    $facets_blocks = array_unique($facets_blocks);
-    // Build the facets blocks found for the current request and update.
-    foreach ($facets_blocks as $block_id => $block_selector) {
-      $block_entity = $this->storage->load($block_id);
-      if ($block_entity) {
-        // Damn Lazy builder!
-        $render_array  = \Drupal\block\BlockViewBuilder::preRender(\Drupal\block\BlockViewBuilder::lazyBuilder($block_id, 'full'));
-        if (isset($render_array['content'][0]['#attached']) && is_array($render_array['content'][0]['#attached'])) {
-          $response->addAttachments($render_array['content'][0]['#attached']);
-        }
-        if (isset($render_array['content']['#attached']) && is_array($render_array['content']['#attached'])) {
-          $response->addAttachments($render_array['content']['#attached']);
-        }
-        if (isset($render_array['#attached']) && is_array($render_array['#attached'])) {
-          $response->addAttachments($render_array['#attached']);
-        }
-      }
-    }
     return $response;
   }
-
 }
