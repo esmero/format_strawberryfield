9ee79d6550ed8f0e591e927b0d6ba8a015e606ad
Diego Pino Navarro <DiegoPino@users.noreply.github.com>
2025-12-03 14:16:18 -0500

    Revert "Update typed properties for RangedRemoteFileRespone to match new from BinaryFileResponse"
    This reverts commit 1628808e9b3653d929a01a4b5de5dde9f4495290.

--- src/Component/HttpFoundation/RangedRemoteFileRespone.php
+++ src/Component/HttpFoundation/RangedRemoteFileRespone.php
@@ -9,16 +9,16 @@ use Symfony\Component\HttpFoundation\Request;
 
 class RangedRemoteFileRespone extends BinaryFileResponse {
 
-  protected static bool $trustXSendfileTypeHeader = false;
+  protected static $trustXSendfileTypeHeader = false;
 
   /**
    * @var File
    */
-  protected File $file;
-  protected int $offset = 0;
-  protected int $end = 0;
-  protected int $maxlen = -1;
-  protected bool $deleteFileAfterSend = false;
+  protected $file;
+  protected $offset = 0;
+  protected $end = 0;
+  protected $maxlen = -1;
+  protected $deleteFileAfterSend = false;
 
   /**
    * {@inheritdoc}
